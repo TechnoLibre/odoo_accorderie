90f96dbc863379ca0c4a0d8780154177cc093b64
Mathieu Benoit <mathben@technolibre.ca>
2020-11-28 22:41:34 -0500

    WIP add portal from project

--- test/__manifest__.py
+++ test/__manifest__.py
@@ -6,6 +6,7 @@
         'security/ir.model.access.csv',
         'views/test_model.xml',
         'views/test_model_2.xml',
+        'views/test_portal_templates.xml',
         'views/menues.xml'
     ],
     'installable': True,
