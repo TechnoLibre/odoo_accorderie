9dbcb1d4ed87cb7860075b599a5aa3e8a9f20216
Mathieu Benoit <mathben@technolibre.ca>
2020-11-28 22:39:27 -0500

    [TEST] module test creation

---
+++ test/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'test',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/test_model.xml',
+        'views/menues.xml'
+    ],
+    'installable': True,
+}
---
+++ test/models/test_model.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, models, fields
+
+
+class TestModel(models.Model):
+    _name = 'test_model'
+    _description = 'test_model'
+
+    name = fields.Char(
+        string='Name',
+        copy=False,
+    )
+
+    variable_1 = fields.Boolean(
+        string='Variable',
+    )
