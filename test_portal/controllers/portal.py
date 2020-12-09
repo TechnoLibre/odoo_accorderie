89772651ab4a8407aefb237d5763eeb3cd46ecb3
Mathieu Benoit <mathben@technolibre.ca>
2020-12-08 23:06:11 -0500

    [ADD] WIP progress test_portal view list

--- test_portal/controllers/portal.py
+++ test_portal/controllers/portal.py
@@ -19,124 +19,36 @@ class CustomerPortal(CustomerPortal):
         return values
 
     # ------------------------------------------------------------
-    # My Project
+    # My Test Model_2 Portal
     # ------------------------------------------------------------
-    def _project_get_page_view_values(self, project, access_token, **kwargs):
+    def _test_model_2_portal_get_page_view_values(self, test_model_2_portal, access_token, **kwargs):
         values = {
-            'page_name': 'project',
-            'project': project,
+            'page_name': 'test_model_2_portal',
+            'test_model_2_portal': test_model_2_portal,
+            'user': request.env.user
         }
-        return self._get_page_view_values(project, access_token, values, 'my_projects_history', False, **kwargs)
+        return self._get_page_view_values(test_model_2_portal, access_token, values, 'my_test_model_2_portals_history',
+                                          False, **kwargs)
 
-    @http.route(['/my/projects', '/my/projects/page/<int:page>'], type='http', auth="user", website=True)
-    def portal_my_projects(self, page=1, date_begin=None, date_end=None, sortby=None, **kw):
+    @http.route(['/my/test_model_2_portals', '/my/test_model_2_portals/page/<int:page>'], type='http', auth="user",
+                website=True)
+    def portal_my_test_model_2_portals(self, page=1, date_begin=None, date_end=None, sortby=None, filterby=None,
+                                       search=None, search_in='content', **kw):
         values = self._prepare_portal_layout_values()
-        Project = request.env['test.model.portal']
+        TestModel2Portal = request.env['test.model_2.portal']
         domain = []
 
         searchbar_sortings = {
             'date': {'label': _('Newest'), 'order': 'create_date desc'},
             'name': {'label': _('Name'), 'order': 'name'},
         }
-        if not sortby:
-            sortby = 'date'
-        order = searchbar_sortings[sortby]['order']
-
-        # archive groups - Default Group By 'create_date'
-        archive_groups = self._get_archive_groups('test.model.portal', domain)
-        if date_begin and date_end:
-            domain += [('create_date', '>', date_begin), ('create_date', '<=', date_end)]
-        # projects count
-        project_count = Project.search_count(domain)
-        # pager
-        pager = portal_pager(
-            url="/my/projects",
-            url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby},
-            total=project_count,
-            page=page,
-            step=self._items_per_page
-        )
-
-        # content according to pager and archive selected
-        projects = Project.search(domain, order=order, limit=self._items_per_page, offset=pager['offset'])
-        request.session['my_projects_history'] = projects.ids[:100]
-
-        values.update({
-            'date': date_begin,
-            'date_end': date_end,
-            'projects': projects,
-            'page_name': 'project',
-            'archive_groups': archive_groups,
-            'default_url': '/my/projects',
-            'pager': pager,
-            'searchbar_sortings': searchbar_sortings,
-            'sortby': sortby
-        })
-        return request.render("project.portal_my_projects", values)
-
-    @http.route(['/my/project/<int:project_id>'], type='http', auth="public", website=True)
-    def portal_my_project(self, project_id=None, access_token=None, **kw):
-        try:
-            project_sudo = self._document_check_access('test.model.portal', project_id, access_token)
-        except (AccessError, MissingError):
-            return request.redirect('/my')
-
-        values = self._project_get_page_view_values(project_sudo, access_token, **kw)
-        return request.render("project.portal_my_project", values)
-
-    # ------------------------------------------------------------
-    # My Task
-    # ------------------------------------------------------------
-    def _task_get_page_view_values(self, task, access_token, **kwargs):
-        values = {
-            'page_name': 'task',
-            'task': task,
-            'user': request.env.user
-        }
-        return self._get_page_view_values(task, access_token, values, 'my_tasks_history', False, **kwargs)
-
-    @http.route(['/my/tasks', '/my/tasks/page/<int:page>'], type='http', auth="user", website=True)
-    def portal_my_tasks(self, page=1, date_begin=None, date_end=None, sortby=None, filterby=None, search=None,
-                        search_in='content', groupby='project', **kw):
-        values = self._prepare_portal_layout_values()
-        searchbar_sortings = {
-            'date': {'label': _('Newest'), 'order': 'create_date desc'},
-            'name': {'label': _('Title'), 'order': 'name'},
-            # 'stage': {'label': _('Stage'), 'order': 'stage_id'},
-            # 'update': {'label': _('Last Stage Update'), 'order': 'date_last_stage_update desc'},
-        }
         searchbar_filters = {
             'all': {'label': _('All'), 'domain': []},
         }
         searchbar_inputs = {
-            # 'content': {'input': 'content', 'label': _('Search <span class="nolabel"> (in Content)</span>')},
-            # 'message': {'input': 'message', 'label': _('Search in Messages')},
-            # 'customer': {'input': 'customer', 'label': _('Search in Customer')},
-            # 'stage': {'input': 'stage', 'label': _('Search in Stages')},
-            'all': {'input': 'all', 'label': _('Search in All')},
         }
         searchbar_groupby = {
-            'none': {'input': 'none', 'label': _('None')},
-            # 'project': {'input': 'project', 'label': _('Project')},
         }
-
-        # extends filterby criteria with project the customer has access to
-        projects = request.env['test.model.portal'].search([])
-        for project in projects:
-            searchbar_filters.update({
-                str(project.id): {'label': project.name, 'domain': [('project_id', '=', project.id)]}
-            })
-
-        # extends filterby criteria with project (criteria name is the project id)
-        # Note: portal users can't view projects they don't follow
-        project_groups = request.env['project.task'].read_group([('project_id', 'not in', projects.ids)],
-                                                                ['project_id'], ['project_id'])
-        for group in project_groups:
-            proj_id = group['project_id'][0] if group['project_id'] else False
-            proj_name = group['project_id'][1] if group['project_id'] else _('Others')
-            searchbar_filters.update({
-                str(proj_id): {'label': proj_name, 'domain': [('project_id', '=', proj_id)]}
-            })
 
         # default sort by value
         if not sortby:
@@ -147,75 +59,153 @@ class CustomerPortal(CustomerPortal):
             filterby = 'all'
         domain = searchbar_filters[filterby]['domain']
 
+        # search
+        if search and search_in:
+            search_domain = []
+            domain += search_domain
         # archive groups - Default Group By 'create_date'
         archive_groups = self._get_archive_groups('test.model_2.portal', domain)
         if date_begin and date_end:
             domain += [('create_date', '>', date_begin), ('create_date', '<=', date_end)]
-
-        # search
-        if search and search_in:
-            search_domain = []
-            if search_in in ('content', 'all'):
-                search_domain = OR([search_domain, ['|', ('name', 'ilike', search), ('description', 'ilike', search)]])
-            if search_in in ('customer', 'all'):
-                search_domain = OR([search_domain, [('partner_id', 'ilike', search)]])
-            if search_in in ('message', 'all'):
-                search_domain = OR([search_domain, [('message_ids.body', 'ilike', search)]])
-            if search_in in ('stage', 'all'):
-                search_domain = OR([search_domain, [('stage_id', 'ilike', search)]])
-            domain += search_domain
-
-        # task count
-        task_count = request.env['test.model_2.portal'].search_count(domain)
+        # test_model_2_portals count
+        test_model_2_portal_count = TestModel2Portal.search_count(domain)
         # pager
         pager = portal_pager(
-            url="'/my/tasks'",
+            url="/my/test_model_2_portals",
             url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby, 'filterby': filterby,
                       'search_in': search_in, 'search': search},
-            total=task_count,
+            total=test_model_2_portal_count,
             page=page,
             step=self._items_per_page
         )
         # content according to pager and archive selected
-        if groupby == 'project':
-            order = "project_id, %s" % order  # force sort on project first to group by project in view
-        tasks = request.env['project.task'].search(domain, order=order, limit=self._items_per_page,
-                                                   offset=(page - 1) * self._items_per_page)
-        request.session['my_tasks_history'] = tasks.ids[:100]
-        if groupby == 'project':
-            grouped_tasks = [request.env['test.model_2.portal'].concat(*g) for k, g in
-                             groupbyelem(tasks, itemgetter('project_id'))]
-        else:
-            grouped_tasks = [tasks]
+
+        # content according to pager and archive selected
+        test_model_2_portals = TestModel2Portal.search(domain, order=order, limit=self._items_per_page,
+                                                       offset=pager['offset'])
+        request.session['my_test_model_2_portals_history'] = test_model_2_portals.ids[:100]
 
         values.update({
             'date': date_begin,
             'date_end': date_end,
-            'grouped_tasks': grouped_tasks,
-            'page_name': 'task',
+            'test_model_2_portals': test_model_2_portals,
+            'page_name': 'test_model_2_portal',
             'archive_groups': archive_groups,
-            'default_url': '/my/tasks',
+            'default_url': '/my/test_model_2_portals',
             'pager': pager,
             'searchbar_sortings': searchbar_sortings,
             'searchbar_groupby': searchbar_groupby,
             'searchbar_inputs': searchbar_inputs,
             'search_in': search_in,
-            'sortby': sortby,
-            'groupby': groupby,
             'searchbar_filters': OrderedDict(sorted(searchbar_filters.items())),
+            'sortby': sortby,
             'filterby': filterby,
         })
-        return request.render("project.portal_my_tasks", values)
+        return request.render("test_portal.portal_my_test_model_2_portals", values)
 
-    @http.route(['/my/task/<int:task_id>'], type='http', auth="public", website=True)
-    def portal_my_task(self, task_id, access_token=None, **kw):
+    @http.route(['/my/test_model_2_portal/<int:test_model_2_portal_id>'], type='http', auth="public", website=True)
+    def portal_my_test_model_2_portal(self, test_model_2_portal_id=None, access_token=None, **kw):
         try:
-            task_sudo = self._document_check_access('test.model_2.portal', task_id, access_token)
+            test_model_2_portal_sudo = self._document_check_access('test.model_2.portal', test_model_2_portal_id,
+                                                                   access_token)
         except (AccessError, MissingError):
             return request.redirect('/my')
 
-        # ensure attachment are accessible with access token inside template
-        for attachment in task_sudo.attachment_ids:
-            attachment.generate_access_token()
-        values = self._task_get_page_view_values(task_sudo, access_token, **kw)
-        return request.render("project.portal_my_task", values)
+        values = self._test_model_2_portal_get_page_view_values(test_model_2_portal_sudo, access_token, **kw)
+        return request.render("test_portal.portal_my_test_model_2_portal", values)
+
+    # ------------------------------------------------------------
+    # My Test Model Portal
+    # ------------------------------------------------------------
+    def _test_model_portal_get_page_view_values(self, test_model_portal, access_token, **kwargs):
+        values = {
+            'page_name': 'test_model_portal',
+            'test_model_portal': test_model_portal,
+            'user': request.env.user
+        }
+        return self._get_page_view_values(test_model_portal, access_token, values, 'my_test_model_portals_history',
+                                          False, **kwargs)
+
+    @http.route(['/my/test_model_portals', '/my/test_model_portals/page/<int:page>'], type='http', auth="user",
+                website=True)
+    def portal_my_test_model_portals(self, page=1, date_begin=None, date_end=None, sortby=None, filterby=None,
+                                     search=None, search_in='content', **kw):
+        values = self._prepare_portal_layout_values()
+        TestModelPortal = request.env['test.model.portal']
+        domain = []
+
+        searchbar_sortings = {
+            'date': {'label': _('Newest'), 'order': 'create_date desc'},
+            'name': {'label': _('Name'), 'order': 'name'},
+        }
+        searchbar_filters = {
+            'all': {'label': _('All'), 'domain': []},
+        }
+        searchbar_inputs = {
+        }
+        searchbar_groupby = {
+        }
+
+        # default sort by value
+        if not sortby:
+            sortby = 'date'
+        order = searchbar_sortings[sortby]['order']
+        # default filter by value
+        if not filterby:
+            filterby = 'all'
+        domain = searchbar_filters[filterby]['domain']
+
+        # search
+        if search and search_in:
+            search_domain = []
+            domain += search_domain
+        # archive groups - Default Group By 'create_date'
+        archive_groups = self._get_archive_groups('test.model.portal', domain)
+        if date_begin and date_end:
+            domain += [('create_date', '>', date_begin), ('create_date', '<=', date_end)]
+        # test_model_portals count
+        test_model_portal_count = TestModelPortal.search_count(domain)
+        # pager
+        pager = portal_pager(
+            url="/my/test_model_portals",
+            url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby, 'filterby': filterby,
+                      'search_in': search_in, 'search': search},
+            total=test_model_portal_count,
+            page=page,
+            step=self._items_per_page
+        )
+        # content according to pager and archive selected
+
+        # content according to pager and archive selected
+        test_model_portals = TestModelPortal.search(domain, order=order, limit=self._items_per_page,
+                                                    offset=pager['offset'])
+        request.session['my_test_model_portals_history'] = test_model_portals.ids[:100]
+
+        values.update({
+            'date': date_begin,
+            'date_end': date_end,
+            'test_model_portals': test_model_portals,
+            'page_name': 'test_model_portal',
+            'archive_groups': archive_groups,
+            'default_url': '/my/test_model_portals',
+            'pager': pager,
+            'searchbar_sortings': searchbar_sortings,
+            'searchbar_groupby': searchbar_groupby,
+            'searchbar_inputs': searchbar_inputs,
+            'search_in': search_in,
+            'searchbar_filters': OrderedDict(sorted(searchbar_filters.items())),
+            'sortby': sortby,
+            'filterby': filterby,
+        })
+        return request.render("test_portal.portal_my_test_model_portals", values)
+
+    @http.route(['/my/test_model_portal/<int:test_model_portal_id>'], type='http', auth="public", website=True)
+    def portal_my_test_model_portal(self, test_model_portal_id=None, access_token=None, **kw):
+        try:
+            test_model_portal_sudo = self._document_check_access('test.model.portal', test_model_portal_id,
+                                                                 access_token)
+        except (AccessError, MissingError):
+            return request.redirect('/my')
+
+        values = self._test_model_portal_get_page_view_values(test_model_portal_sudo, access_token, **kw)
+        return request.render("test_portal.portal_my_test_model_portal", values)
